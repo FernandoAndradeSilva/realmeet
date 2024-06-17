e947bcfb4adeae243293c31815b7f545891f4783
FernandoAndradeSilva <fernando.andrade.inf@gmail.com>
2024-06-17 02:02:27 -0300

    put-database

--- service/src/main/java/br/com/fernando/realmeet/RoomController.java
+++ service/src/main/java/br/com/fernando/realmeet/RoomController.java
@@ -1,19 +1,10 @@
 package br.com.fernando.realmeet;
 
-import static java.util.concurrent.CompletableFuture.supplyAsync;
-import javax.validation.Valid;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import br.com.fernando.realmeet.api.facade.RoomsApi;
-import br.com.fernando.realmeet.api.model.Room;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
-public class RoomController implements RoomsApi {
+public class RoomController {
 
-    @Override
-    public CompletableFuture<ResponseEntity<Room>> listRooms(@Valid Long id) {
-        return supplyAsync(() -> ResponseEntity.ok( new Room().id(1L).name("Room #1")));
-    }
+
 }
