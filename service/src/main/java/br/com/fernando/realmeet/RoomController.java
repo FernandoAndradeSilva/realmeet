6b347108982b6233b814c113a838b5eb35f95da7
FernandoAndradeSilva <fernando.andrade.inf@gmail.com>
2024-06-18 23:26:06 -0300

    1 controller

--- service/src/main/java/br/com/fernando/realmeet/RoomController.java
+++ service/src/main/java/br/com/fernando/realmeet/RoomController.java
@@ -1,10 +1,20 @@
 package br.com.fernando.realmeet;
 
 
+import br.com.fernando.realmeet.api.facade.RoomsApi;
+import br.com.fernando.realmeet.api.model.Room;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-public class RoomController {
+public class RoomController implements RoomsApi {
+
+    @Override
+    public ResponseEntity<Room> listRooms(@Valid Long id) {
+        return RoomsApi.super.listRooms(id);
+    }
 
 
 }
